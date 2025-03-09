db01d28006ac9f9bb640addcb8c7caaff72d8b0a
usermicrodevices <29286243+usermicrodevices@users.noreply.github.com>
2025-03-10 01:50:18 +0300

    add flipping to products dialog

--- src/ui/dialog_products.py
+++ src/ui/dialog_products.py
@@ -27,18 +27,21 @@ class ProductsDialog(ft.AlertDialog):
         page = kwargs.pop('page')
         super().__init__(*args, **kwargs)
         #self.title = ft.TextField('products')
-        captions = [
-            Caption(title='id', numeric=True),
-            Caption(title='article'),
-            Caption(title='name'),
-            Caption(title='price', numeric=True),
-            Caption(title='barcodes')
-        ]
-        products, msg = page.db_conn.get_products()
+        self.limit = 10
+        self.offset = 0
+        self.products_count, msg = page.db_conn.get_products_count()
+        products, msg = page.db_conn.get_products(limit=10, offset=0)
         if not products:
             self.content = ft.Text(msg)
         else:
-            self.content = ft.DataTable(
+            captions = [
+                Caption(title='id', numeric=True),
+                Caption(title='article'),
+                Caption(title='name'),
+                Caption(title='price', numeric=True),
+                Caption(title='barcodes')
+            ]
+            self.data_table = ft.DataTable(
                 show_checkbox_column = True,
                 column_spacing = 5,
                 border_radius = 5,
@@ -47,6 +50,14 @@ class ProductsDialog(ft.AlertDialog):
                 horizontal_lines = ft.BorderSide(2, 'green'),
                 columns = captions,
                 rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(d['id'])), ft.DataCell(ft.Text(d['article'][:10])), ft.DataCell(ft.Text(d['name'][:10])), ft.DataCell(ft.Text(d['price'])), ft.DataCell(ft.Text('\n'.join(d['barcodes'])))], on_select_changed=self.on_select, data=d) for d in products]
+            )
+            self.content = ft.Column(controls=[
+                ft.Row([self.data_table]),
+                ft.Row([
+                        ft.ElevatedButton('Prev', on_click=self.handle_prev),
+                        ft.ElevatedButton('Next', on_click=self.handle_next)
+                    ])
+                ]
             )
         self.actions = [
             DialogAction('select', is_ok=True, on_click=self.handle_action_click),
@@ -64,3 +75,23 @@ class ProductsDialog(ft.AlertDialog):
         if evt.control.is_ok:
             self.page.sync_products()
         self.page.close(evt.control.parent)
+
+    def handle_prev(self, evt):
+        self.products_count, msg = self.page.db_conn.get_products_count()
+        if self.offset >= self.products_count or self.offset > 0:
+            self.offset -= self.limit
+            if self.offset < 0:
+                self.offset = 0
+            products, msg = self.page.db_conn.get_products(limit=self.limit, offset=self.offset)
+            if products:
+                self.data_table.rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(d['id'])), ft.DataCell(ft.Text(d['article'][:10])), ft.DataCell(ft.Text(d['name'][:10])), ft.DataCell(ft.Text(d['price'])), ft.DataCell(ft.Text('\n'.join(d['barcodes'])))], on_select_changed=self.on_select, data=d) for d in products]
+                self.data_table.update()
+
+    def handle_next(self, evt):
+        self.products_count, msg = self.page.db_conn.get_products_count()
+        if self.offset < self.products_count:
+            self.offset += self.limit
+            products, msg = self.page.db_conn.get_products(limit=self.limit, offset=self.offset)
+            if products:
+                self.data_table.rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(d['id'])), ft.DataCell(ft.Text(d['article'][:10])), ft.DataCell(ft.Text(d['name'][:10])), ft.DataCell(ft.Text(d['price'])), ft.DataCell(ft.Text('\n'.join(d['barcodes'])))], on_select_changed=self.on_select, data=d) for d in products]
+                self.data_table.update()
