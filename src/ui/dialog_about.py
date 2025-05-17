6ac8d3ff7af848aace0482599165c32bcfd0a74a
usermicrodevices <29286243+usermicrodevices@users.noreply.github.com>
2025-05-17 09:56:22 +0300

    add order to provider

--- src/ui/dialog_about.py
+++ src/ui/dialog_about.py
@@ -1,6 +1,6 @@
 import flet as ft
 from log_tools import *
-from translation import _#, set_locale
+from translation import _
 
 
 VERSION = ''
@@ -22,7 +22,6 @@ class AboutDialog(ft.AlertDialog):
     def __init__(self, *args, **kwargs):
         page = kwargs.pop('page')
         super().__init__(*args, **kwargs)
-        #set_locale(page.client_storage.get('translation_language'))
         CONTENT = f'''{_("Version")} {VERSION}\n''' + \
             f'''Esc - {_("close any dialog")}\n''' + \
             f'''F1 - {_("this dialog")}\n''' + \
@@ -31,6 +30,7 @@ class AboutDialog(ft.AlertDialog):
             f'''F3 - {_("switch between search and final sum")}\n''' + \
             f'''F4 - {_("switch between search and count last product")}\n''' + \
             f'''F5 - {_("switch between search and scanning")}\n''' + \
+            f'''F10 - {_("finish order provider")}\n''' + \
             f'''F11 - {_("finish order customer")}\n''' + \
             f'''F12 - {_("finish sale")}\n''' + \
             f'''(flet {_("version")} {ft.version.version})'''
