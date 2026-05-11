3d36ff51c0f65e2f47eb2f8df4547004446967e5
fir <29286243+usermicrodevices@users.noreply.github.com>
2026-05-11 16:35:41 +0300

    extend language pair parsing in string parameter

--- src/translation.py
+++ src/translation.py
@@ -3,6 +3,17 @@ import gettext, locale, logging, os, re
 global gettext_catalog
 gettext_catalog = None
 
+pattern = re.compile(r"""^\s*([\(\[])\s*['"]?(\S+?)['"]?\s*,\s*['"]?(\S+?)['"]?\s*([\)\]])\s*$""", re.VERBOSE)
+
+def match_pair(s):
+    m = pattern.match(s)
+    if not m:
+        return None
+    open_b, v1, v2, close_b = m.group(1), m.group(2), m.group(3), m.group(4)
+    if (open_b == '(' and close_b == ')') or (open_b == '[' and close_b == ']'):
+        return v1, v2
+    return None
+
 def set_locale(current_locale=None, encoding=None, locale_dir='locale'):
     global gettext_catalog
     if not os.path.isdir(locale_dir):
@@ -14,17 +25,13 @@ def set_locale(current_locale=None, encoding=None, locale_dir='locale'):
     if isinstance(current_locale, (list, tuple)):
         current_locale, encoding = current_locale
     else:
-        res = re.match(r"\('(\S+)', '(\S+)'\)", current_locale) or re.match(r"\['(\S+)', '(\S+)'\]", current_locale)
-        if res:
-            grps = res.groups()
-            if len(grps) == 2:
-                current_locale, encoding = grps
+        current_locale, encoding = match_pair(current_locale)
     new_locale = f'{current_locale}.{encoding}' if current_locale else ''
     try:
         locale.setlocale(locale.LC_ALL, new_locale)
         locale.setlocale(locale.LC_CTYPE, new_locale)
-    except Exception as e:
-        logging.error(['🆘', __name__, new_locale, e, f'🇱🇮🇳🇪{e.__traceback__.tb_lineno}'])
+    except Exception as err:
+        logging.error(['🆘', f'{__name__}.py', new_locale, err, f'🇱🇮🇳🇪{err.__traceback__.tb_lineno}'])
         return False
     else:
         lang = gettext.translation('prod', locale_dir, languages=[current_locale])
